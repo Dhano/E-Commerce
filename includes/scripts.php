6c95f1182a6bb2ffdecfe43439dc0e998b44536b
Nikhil Shadija <user@NIKS.Niks>
2019-02-06 15:22:28 +0530

    Basic UI Updated

---
+++ includes/scripts.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: NIKHIL SHADIJA
+ * Date: 2/6/2019
+ * Time: 1:25 PM
+ */
+?>
+
+<!-- jQuery (Necessary for All JavaScript Plugins) -->
+<script src="assets/js/jquery/jquery-2.2.4.min.js"></script>
+<!-- Popper js -->
+<script src="assets/js/popper.min.js"></script>
+<!-- Bootstrap js -->
+<script src="assets/js/bootstrap.min.js"></script>
+<!-- Plugins js -->
+<script src="assets/js/plugins.js"></script>
+<!-- Classy Nav js -->
+<script src="assets/js/classy-nav.min.js"></script>
+<!-- Active js -->
+<script src="assets/js/active.js"></script>
