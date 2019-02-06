b6712ed51206e623d8003fddf5ea6a388c026423
Dhano <dhananjay62.dg@gmail.com>
2019-02-06 15:59:02 +0530

    Updated sql

--- ecom.sql
+++ ecom.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 06, 2019 at 11:22 AM
+-- Generation Time: Feb 06, 2019 at 11:28 AM
 -- Server version: 10.1.37-MariaDB
 -- PHP Version: 7.3.1
 
